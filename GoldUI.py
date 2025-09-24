513e592f0e2c026b5a5f2fad67d0c333f21d397c
bmckay18 <106320005+bmckay18@users.noreply.github.com>
2025-09-24 21:44:37 +1000

    updated variable names to snake_case

--- GoldUI.py
+++ GoldUI.py
@@ -6,21 +6,21 @@ from config import *
 class GoldUI():
     def __init__(self, root = None):
         self.root = root
-        self.logicCB = lambda *args, **kwargs: None
+        self.logic_cb = lambda *args, **kwargs: None
     
     def update_gold(self, amount):
-        self.goldLabel.config(text = f'Gold: {amount:,}')
+        self.gold_label.config(text = f'Gold: {amount:,}')
     
-    def place_widgets(self, widgetRow):
+    def place_widgets(self, widget_row):
         # Create widgets
-        self.goldLabel = tk.Label(self.root)
-        self.goldEntry = tk.Entry(self.root)
-        self.goldButton = tk.Button(self.root, text = 'Update Gold', command = self.update_gold_cmd)
+        self.gold_label = tk.Label(self.root)
+        self.gold_entry = tk.Entry(self.root)
+        self.gold_button = tk.Button(self.root, text = 'Update Gold', command = self.update_gold_cmd)
 
         # Place widgets
-        self.goldLabel.grid(row = widgetRow, column = 0, padx = 5, pady = 5)
-        self.goldEntry.grid(row = widgetRow, column = 1)
-        self.goldButton.grid(row = widgetRow + 1, column = 1, padx = 5)
+        self.gold_label.grid(row = widget_row, column = 0, padx = 5, pady = 5)
+        self.gold_entry.grid(row = widget_row, column = 1)
+        self.gold_button.grid(row = widget_row + 1, column = 1, padx = 5)
 
         # Update the label
         self.update_gold(0)
@@ -29,14 +29,14 @@ class GoldUI():
         self.root = root
     
     def add_logic_cb(self, func):
-        self.logicCB = func 
+        self.logic_cb = func 
     
     def _notify_logic_cb(self):
-        self.logicCB(self.goldEntry.get())
+        self.logic_cb(self.gold_entry.get())
 
     def update_gold_cmd(self):
         try:
             self._notify_logic_cb()
-            self.goldEntry.delete(0, tk.END) 
+            self.gold_entry.delete(0, tk.END) 
         except ValueError as m:
             messagebox.showerror('Error', f'An error occurred: {m}')
