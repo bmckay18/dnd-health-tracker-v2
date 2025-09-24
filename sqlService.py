be9492238290de51e050bf83432dc4300f644b78
bmckay18 <106320005+bmckay18@users.noreply.github.com>
2025-09-24 21:46:14 +1000

    updated naming conventions

--- sqlService.py
+++ sqlService.py
@@ -2,19 +2,19 @@ import sqlite3 as s
 from config import *
 
 class SQLService():
-    db_filename = database_name
+    DB_FILE_NAME = database_name
 
     def __init__(self):
         self.db_schema = base_path / "db" / "schema.sql"
 
-    def execute_insert(self, query, returnFlag: int = 0): # Used for insert queries
-        conn = s.connect(self.db_filename)
+    def execute_insert(self, query, return_flag: int = 0): # Used for insert queries
+        conn = s.connect(self.DB_FILE_NAME)
         cursor = conn.cursor()
         try:
-            if returnFlag == 0:
+            if return_flag == 0:
                 cursor.executescript(query)
                 conn.commit()
-            elif returnFlag == 1:
+            elif return_flag == 1:
                 cursor.execute(query)
                 last_id = cursor.lastrowid
                 conn.commit()
@@ -26,7 +26,7 @@ class SQLService():
             conn.close()
     
     def execute_update(self, query): # Used for update queries
-        conn = s.connect(self.db_filename)
+        conn = s.connect(self.DB_FILE_NAME)
         cursor = conn.cursor()
         try:
             cursor.executescript(query)
@@ -38,7 +38,7 @@ class SQLService():
             conn.close()
     
     def execute_select(self, query): # Used for select queries
-        conn = s.connect(self.db_filename)
+        conn = s.connect(self.DB_FILE_NAME)
         cursor = conn.cursor()
         try:
             cursor.execute(query)
@@ -50,6 +50,6 @@ class SQLService():
             conn.close()
     
     def create_db(self):
-        with s.connect(self.db_filename) as conn:
+        with s.connect(self.DB_FILE_NAME) as conn:
             with open(self.db_schema, 'r') as f:
                 conn.executescript(f.read())
