fe8977785cba1c219be1620159e9d0db198a0b44
bmckay18 <106320005+bmckay18@users.noreply.github.com>
2025-09-24 21:38:34 +1000

    renamed all variablesl to snake_case

--- InventoryWindow.py
+++ InventoryWindow.py
@@ -13,74 +13,74 @@ class InventoryWindow():
         self.window.geometry("648x374")
         self.window.title("Player Inventory")
         self.page = 0
-        self.maxPage = 0
-        self.itemUIs = []
+        self.max_page = 0
+        self.item_uis = []
         self.padx = 2.5
         self.pady = 2.5
-        self.displayedItemsLimit = 10 # Number of items to display per page
-        self.newInventoryCallback = lambda *args, **kwargs: None
+        self.displayed_items_limit = 10 # Number of items to display per page
+        self.new_inventory_cb = lambda *args, **kwargs: None
     
     def create_ui_instances(self, items: list[Inventory]):
         for item in items:
-            self.itemUIs.append(InventoryUI(self.itemFrame, item))
+            self.item_uis.append(InventoryUI(self.item_frame, item))
             
         self.set_max_page_limit()
     
     def set_max_page_limit(self):
-        self.maxPage = m.ceil(len(self.itemUIs) / self.displayedItemsLimit)
+        self.max_page = m.ceil(len(self.item_uis) / self.displayed_items_limit)
     
     def create_modal_frames(self):
-        self.itemFrame = tk.Frame(self.window)
-        self.editFrame = tk.Frame(self.window) 
+        self.item_frame = tk.Frame(self.window)
+        self.edit_frame = tk.Frame(self.window) 
 
         # Create edit frame widgets
-        self.itemNameEntry = tk.Entry(self.editFrame)
-        self.itemNameLabel = tk.Label(self.editFrame, text = 'Name:')
+        self.item_name_entry = tk.Entry(self.edit_frame)
+        self.item_name_label = tk.Label(self.edit_frame, text = 'Name:')
 
-        self.itemQuantityEntry = tk.Entry(self.editFrame)
-        self.itemQuantityLabel = tk.Label(self.editFrame, text = 'Quantity:')
+        self.item_quantity_entry = tk.Entry(self.edit_frame)
+        self.item_quantity_label = tk.Label(self.edit_frame, text = 'Quantity:')
 
-        self.createItemButton = tk.Button(self.editFrame, text = 'Add', command = self._insert_inventory_item)
+        self.create_item_button = tk.Button(self.edit_frame, text = 'Add', command = self._insert_inventory_item)
 
-        self.nextPageButton = tk.Button(self.editFrame, text = '>', command = self.next_page)
-        self.prevPageButton = tk.Button(self.editFrame, text = '<', command = self.previous_page)
-        self.pageLabel = tk.Label(self.editFrame, text = f'Page: {self.page + 1}')
-        self.updateQuantitiesButton = tk.Button(self.editFrame, text = 'Update Items', command = self._update_quantities)
+        self.next_page_button = tk.Button(self.edit_frame, text = '>', command = self.next_page)
+        self.prev_page_button = tk.Button(self.edit_frame, text = '<', command = self.previous_page)
+        self.page_label = tk.Label(self.edit_frame, text = f'Page: {self.page + 1}')
+        self.update_quantities_button = tk.Button(self.edit_frame, text = 'Update Items', command = self._update_quantities)
 
         # Place frames
-        self.itemFrame.pack(side='left', fill='both', expand=True)
-        self.editFrame.pack(side='left', fill='y')
+        self.item_frame.pack(side='left', fill='both', expand=True)
+        self.edit_frame.pack(side='left', fill='y')
 
         # Place edit frame widgets
-        self.itemNameLabel.grid(row = 1, column = 1, padx = self.padx, pady = self.pady)
-        self.itemNameEntry.grid(row = 1, column = 2, padx = self.padx, pady = self.pady)
+        self.item_name_label.grid(row = 1, column = 1, padx = self.padx, pady = self.pady)
+        self.item_name_entry.grid(row = 1, column = 2, padx = self.padx, pady = self.pady)
 
-        self.itemQuantityLabel.grid(row = 2, column = 1, padx = self.padx, pady = self.pady)
-        self.itemQuantityEntry.grid(row = 2, column = 2, padx = self.padx, pady = self.pady)
+        self.item_quantity_label.grid(row = 2, column = 1, padx = self.padx, pady = self.pady)
+        self.item_quantity_entry.grid(row = 2, column = 2, padx = self.padx, pady = self.pady)
 
-        self.createItemButton.grid(row = 3, column = 2, pady = self.pady, padx = self.padx)
+        self.create_item_button.grid(row = 3, column = 2, pady = self.pady, padx = self.padx)
 
-        self.updateQuantitiesButton.grid(row = 4, column = 2, pady = self.pady)
+        self.update_quantities_button.grid(row = 4, column = 2, pady = self.pady)
 
-        self.prevPageButton.grid(row = 5, column = 1)
-        self.pageLabel.grid(row = 5, column = 2, pady = self.pady * 2)
-        self.nextPageButton.grid(row = 5, column = 3, padx = self.padx * 2)
+        self.prev_page_button.grid(row = 5, column = 1)
+        self.page_label.grid(row = 5, column = 2, pady = self.pady * 2)
+        self.next_page_button.grid(row = 5, column = 3, padx = self.padx * 2)
 
     def _update_quantities(self):
-        for item in self.itemUIs:
+        for item in self.item_uis:
             item.UpdateDatabase()
     
     def place_item_widgets(self):
-        page_factor = self.page * self.displayedItemsLimit
+        page_factor = self.page * self.displayed_items_limit
 
-        self.pageLabel['text'] = f'Page: {self.page + 1}'
+        self.page_label['text'] = f'Page: {self.page + 1}'
 
-        for ui in self.itemUIs:
+        for ui in self.item_uis:
             ui.HideWidgets()
 
         try:
-            for i in range(0,self.displayedItemsLimit):
-                self.itemUIs[i + page_factor].DisplayWidgets(i)
+            for i in range(0,self.displayed_items_limit):
+                self.item_uis[i + page_factor].DisplayWidgets(i)
         except IndexError as m:
             print(m)
     
@@ -95,21 +95,21 @@ class InventoryWindow():
             self.place_item_widgets()
     
     def next_page(self):
-        if self.page + 1 != self.maxPage:
+        if self.page + 1 != self.max_page:
             self.page += 1
             self.place_item_widgets()
     
     def _insert_inventory_item(self):
-        name = self.itemNameEntry.get()
+        name = self.item_name_entry.get()
         try:
-            quantity = int(self.itemQuantityEntry.get())
-            self.newInventoryCallback(name, quantity)
+            quantity = int(self.item_quantity_entry.get())
+            self.new_inventory_cb(name, quantity)
             self.place_item_widgets()
         except ValueError as m:
             messagebox.showerror("Error", f'An error occurred: {m}')
         finally:
-            self.itemNameEntry.delete(0, tk.END)
-            self.itemQuantityEntry.delete(0, tk.END)
+            self.item_name_entry.delete(0, tk.END)
+            self.item_quantity_entry.delete(0, tk.END)
     
     def add_insert_inventory_callback(self, func):
-        self.newInventoryCallback = func
+        self.new_inventory_cb = func
