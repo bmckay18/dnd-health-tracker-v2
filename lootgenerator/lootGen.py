0da69204d30aa3388ff6652ecb93621d1e8a37ac
bmckay18 <106320005+bmckay18@users.noreply.github.com>
2025-09-26 22:36:56 +1000

    added constructor method and method to retrieve a specific loot table

--- lootgenerator/lootGen.py
+++ lootgenerator/lootGen.py
@@ -1,8 +1,19 @@
 # Define libraries
 from config import *
+from sql_service import SQLService
+from functions.extract_query import extract_query
 
 # This module will handle the logic for generating the loot
 
 # Define class
 class LootGen():
-    pass
+    def __init__(self, loot_table: int):
+        self.conn = SQLService()
+        self.loot_table_id = loot_table
+        self.loot_table = self._get_loot_items()
+    
+    def _get_loot_items(self):
+        query = extract_query('uspRetrieveLootTableFromID')
+        query = query % (self.loot_table_id) # Replace placeholder with magic table
+
+        return self.conn.execute_update(query)
