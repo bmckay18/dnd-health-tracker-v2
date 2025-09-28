3e7fb3bab8b5f5fa1f7b43090ff248c263211742
bmckay18 <106320005+bmckay18@users.noreply.github.com>
2025-09-28 19:19:38 +1000

    added function to increase the current round by 1 and update the database

--- roundtracker/round_tracker.py
+++ roundtracker/round_tracker.py
@@ -5,7 +5,7 @@ from functions.extract_query import extract_query
 class RoundTracker():
     def __init__(self):
         self.conn = SQLService()
-        self.round = self._retrieve_round()
+        self.current_round = self._retrieve_round()
     
     def _retrieve_round(self):
         query = extract_query("uspRetrieveRound")
@@ -14,3 +14,14 @@ class RoundTracker():
         except ValueError as m:
             print(m)
             return -1
+    
+    def next_round(self):
+        try:
+            self.current_round += 1
+            self._update_round()
+        except Exception as m:
+            print(m)
+    
+    def _update_round(self):
+        query = extract_query('uspUpdateRound') % self.current_round
+        self.conn.execute_update(query)
