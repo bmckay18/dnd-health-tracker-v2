cf1d6dac280098fb425266f100bbabc60964db79
bmckay18 <106320005+bmckay18@users.noreply.github.com>
2025-09-28 19:32:53 +1000

    initial commit of round_ui.py

--- roundtracker/round_ui.py
+++ roundtracker/round_ui.py
@@ -2,5 +2,34 @@
 import tkinter as tk
 
 class RoundUI():
-    def __init__(self):
-        pass
+    def __init__(self, master):
+        self.master = master
+        
+        # Initialise tk widgets
+        self.round_label = None 
+        self.next_round_button = None 
+        self.prev_round_button = None
+
+        # Initialise cb functions
+        self.next_round_cb = lambda *args, **kwargs: None
+        self.prev_round_cb = lambda *args, **kwargs: None
+
+    def place_widgets(self, set_row: int):
+        # Create widgets
+        self.round_label = tk.Label(self.master)
+        self.next_round_button = tk.Button(self.master, text = 'Next Round', command = self.next_round_cb())
+        self.prev_round_button = tk.Button(self.master, text = 'Previous Round', command = self.prev_round_cb())
+
+        # Place widgets
+        self.round_label.grid(row = set_row, column = 0)
+        self.next_round_button.grid(row = set_row + 1, column = 0)
+        self.prev_round_button.grid(row = set_row + 2, column = 0)
+    
+    def update_label(self, current_round):
+        self.round_label['text'] = f'Round: {current_round}'
+
+t = tk.Tk()
+a = RoundUI(t)
+a.place_widgets(1)
+a.update_label(5)
+t.mainloop()
