bf9ea4e880426e97f5ec9141d28838e88e8d6a3f
bmckay18 <106320005+bmckay18@users.noreply.github.com>
2025-09-28 19:15:12 +1000

    added function to retrieve current round

--- roundtracker/round_tracker.py
+++ roundtracker/round_tracker.py
@@ -5,3 +5,12 @@ from functions.extract_query import extract_query
 class RoundTracker():
     def __init__(self):
         self.conn = SQLService()
+        self.round = self._retrieve_round()
+    
+    def _retrieve_round(self):
+        query = extract_query("uspRetrieveRound")
+        try:
+            return int(self.conn.execute_select(query))
+        except ValueError as m:
+            print(m)
+            return -1
