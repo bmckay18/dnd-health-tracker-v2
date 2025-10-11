9906ff9c2732e62a32c9ad2ab3fb9e25e04d24a7
bmckay18 <106320005+bmckay18@users.noreply.github.com>
2025-10-11 22:04:45 +1000

    update module init files to import all module classes/functions.

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,4 +1,12 @@
+# Define system libraries
 import sys
 from pathlib import Path 
 
+# Import module files
+from .extract_query import extract_query
+
+# Setup system path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+# Setup module export 
+__all__ = ['extract_query']
--- lootgenerator/__init__.py
+++ lootgenerator/__init__.py
@@ -1,4 +1,14 @@
+# Define system libraries
 import sys
 from pathlib import Path 
 
+# Import module functions
+from .loot_generator import LootGen
+from .loot_ui import LootUI
+from .loot_window_ui import LootWindowUI
+
+# Setup sys path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+# Export module classes
+__all__ = ['LootGen','LootUI','LootWindowUI']
