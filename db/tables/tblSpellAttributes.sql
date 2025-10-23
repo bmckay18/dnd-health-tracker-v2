5653bfe5f5c668cb680638a76c5691bdda670938
bmckay18 <106320005+bmckay18@users.noreply.github.com>
2025-10-23 21:56:29 +1000

    create new spell tables

---
+++ db/tables/tblSpellAttributes.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS "tblSpellAttributes"
+(
+    "colSpellAttributesCharacterID" INTEGER,
+    "colSpellAttributesSpellSlot" INTEGER,
+    "colSpellAttributesSpellSlotMax" INTEGER,
+    "colSpellAttributesSpellSlotCurrent" INTEGER
+);
