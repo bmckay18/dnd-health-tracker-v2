a5c47c281e50a147640f82b27a3b6262663b724a
bmckay18 <106320005+bmckay18@users.noreply.github.com>
2025-10-23 22:09:31 +1000

    added spellslot to module path

--- spelltracker/__init__.py
+++ spelltracker/__init__.py
@@ -3,10 +3,10 @@ import sys
 from pathlib import Path 
 
 # Import module classes
-
+from .spell_slot import SpellSlot
 
 # Setup system path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 # Export modules
-__all__ = []
+__all__ = ['SpellSlot']
