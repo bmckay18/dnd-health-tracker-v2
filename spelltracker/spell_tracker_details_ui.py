9082b2a59a52f783a50767e08cf35b1b2171a315
bmckay18 <106320005+bmckay18@users.noreply.github.com>
2025-10-25 09:54:08 +1000

    created spell tracker details UI class

---
+++ spelltracker/spell_tracker_details_ui.py
@@ -0,0 +1,7 @@
+# Import libraries
+import tkinter as tk
+
+class SpellTrackerDetailsUI():
+    def __init__(self, root, character_logic):
+        self.root = root
+        self.character_logic = character_logic
