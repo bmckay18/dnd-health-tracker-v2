638ec5c2fbe4acf91a3e97242041dfbf8891a33f
bmckay18 <106320005+bmckay18@users.noreply.github.com>
2025-10-26 19:04:38 +1000

    created new class for handling spell widget placement and added functions to create and place widgets in a frame

---
+++ spelltracker/details_ui_spell_widgets.py
@@ -0,0 +1,12 @@
+# Import libraries
+import tkinter as tk 
+from spell_slot import SpellSlot
+
+class DetailsUISpellWidgets():
+    def __init__(self, spell_instance: SpellSlot, root):
+        self.root = root 
+        self.spell_info = spell_instance
+        self.frame = tk.Frame(root)
+    
+    def create_and_place_widgets(self, widget_row):
+        raise NotImplementedError('This has not been implemented')
--- spelltracker/spell_tracker_details_ui.py
+++ spelltracker/spell_tracker_details_ui.py
@@ -1,7 +1,32 @@
 # Import libraries
 import tkinter as tk
+from spell_slot_character import SpellSlotCharacter
+from details_ui_spell_widgets import DetailsUISpellWidgets
 
 class SpellTrackerDetailsUI():
-    def __init__(self, root, character_logic):
+    def __init__(self, root, character_logic: SpellSlotCharacter):
         self.root = root
         self.character_logic = character_logic
+        self.spell_uis = []
+    
+    def create_spell_uis(self):
+        for spell in self.character_logic.spell_slots:
+            self.spell_uis.append(DetailsUISpellWidgets(spell, self.root))
+    
+    def create_widgets(self):
+        # Create widgets
+        self.character_label = tk.Label(self.root, text = self.character_logic.name)
+        self.reset_spells_buttons = tk.Button(self.root, text = 'Long Rest', command = None)
+        self.create_spell_uis()
+
+        # Place widgets
+        widget_row = 1
+        padding = 5
+
+        self.character_label.grid(row = 0, column = 0, pady = padding, padx = padding)
+
+        for spell_ui in self.spell_uis:
+            spell_ui.create_and_place_widgets(widget_row)
+            widget_row += 1
+        
+        self.reset_spells_buttons.grid(row = widget_row, column = 1, pady = padding)
